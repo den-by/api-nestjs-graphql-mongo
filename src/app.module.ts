900d4d67f7f0d55e49a72e7ce3ca4fbd99d7fd0f
denis <denis.da.by@gmail.com>
2021-08-05 14:29:34 +0300

    init

---
+++ src/app.module.ts
@@ -0,0 +1,48 @@
+import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { GraphQLError } from 'graphql';
+import { GqlJwtAuthGuard } from '@/guards/gql-jwt-auth.guard';
+import { RolesGuard } from '@/guards/roles.guard';
+import { AccountsModule } from './accounts/accounts.module';
+import { TransactionsModule } from '@/transactions/transactions.module';
+import { ContactsModule } from '@/contacts/contacts.module';
+import { EntriesModule } from '@/entries/entries.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+    }),
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/graphql.schema.gql'),
+      sortSchema: true,
+      formatError: (error: GraphQLError) => ({
+        message: error.extensions?.exception?.response?.message || error.message,
+      }),
+    }),
+    UsersModule,
+    AuthModule,
+    AccountsModule,
+    TransactionsModule,
+    ContactsModule,
+    EntriesModule,
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: GqlJwtAuthGuard,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: RolesGuard,
+    },
+  ],
+})
+export class AppModule {}
